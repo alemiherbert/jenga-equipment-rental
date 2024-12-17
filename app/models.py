d54e49410ef1c8251a7ff6ad9e363094a88bfb21
Alemi Herbert <alemiherbert@gmail.com>
2024-12-17 15:40:34 +0300

    Add booking model

--- app/models.py
+++ app/models.py
@@ -1,12 +1,15 @@
 """
-Application Modeks
+Application Models
 """
 
 from app import db
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, String, Float
+from sqlalchemy.orm import mapped_column, relationship, Mapped, WriteOnlyMapped
+from sqlalchemy import Integer, String, Float, ForeignKey, DateTime, Enum
 # from sqlalchemy.dialects.postgresql import JSON
+
+import enum
 from typing import Optional
+from datetime import datetime, timezone
 
 
 class User(db.Model):
@@ -39,6 +42,12 @@ class Equipment(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64))
     # images: Mapped[list[str]] = mapped_column(JSON, default=list)
+    location_id: Mapped[Optional[int]] = mapped_column(ForeignKey("location.id"))
+    location: WriteOnlyMapped["Location"] = relationship("Location", back_populates="equipment")
+
+    def __repr__(self):
+        return f"<Equipment {self.name} at Location {self.location.name if self.location else 'N/A'}>"
+
 
 class Location(db.Model):
     """
@@ -55,5 +64,38 @@ class Location(db.Model):
     object_id: Mapped[Optional[int]] = mapped_column(nullable=True)
     object_type: Mapped[Optional[str]] = mapped_column(String(64))
 
+    equipment: WriteOnlyMapped[list["Equipment"]] = relationship("Equipment", back_populates="location")
+
     def __repr__(self):
         return f"<Location {self.name or self.city}>"
+
+
+class Booking(db.Model):
+    """
+    Booking model
+    """
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    equipment_id: Mapped[int] = mapped_column(ForeignKey('equipment.id'))
+    start_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    end_date: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+
+    class Status(enum.Enum):
+        PENDING = "pending"
+        CONFIRMED = "confirmed"
+        CANCELLED = "cancelled"
+        COMPLETED = "completed"
+    
+    status: Mapped[Status] = mapped_column(Enum(Status), default=Status.PENDING.value)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        default=datetime.now(timezone.utc),
+        onupdate=datetime.now(timezone.utc))
+
+    user: WriteOnlyMapped["User"] = relationship("User", back_populates="bookings")
+    equipment: WriteOnlyMapped["Equipment"] = relationship("Equipment", back_populates="bookings")
+
+    def __repr__(self):
+        equipment_name = self.equipment.name if self.equipment else "N/A"
+        user_name = self.user.name if self.user else "N/A"
+        return f"<Booking {self.id} - {equipment_name} by {user_name}>"
