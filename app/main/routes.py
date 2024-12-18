d067eab64987e65ac942d8be7913f32b58ff21e3
Alemi Herbert <alemiherbert@gmail.com>
2024-12-18 14:53:24 +0300

    Test automatic user loader integration

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,17 @@ Main app routes
 
 from app.main import main
 from flask import jsonify
-
+from flask_jwt_extended import jwt_required, current_user
 @main.route("/")
 def index():
     return jsonify({"greeting": "Hello, World!"})
+
+
+@main.route("/clandestine")
+@jwt_required()
+def clandestine():
+    return jsonify(
+        id=current_user.id,
+        name=current_user.name,
+        email=current_user.email,
+    )
